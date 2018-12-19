46fb31aa9397e8644cae72152722f55510ded37c
Waqar Mohammad <waqar@waqar.co>
2018-12-19 11:40:34 +0000

    4/7 excercises completed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    str = str;
+    var result = '';
 
+    if(num > 0){
+        for (i = 1; i < num + 1; i++) {
+            result += str;
+        }
+    } else if (num === 0){
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    }
+    return result;
 }
-
 module.exports = repeatString
