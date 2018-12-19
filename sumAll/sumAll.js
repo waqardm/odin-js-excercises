d7a06cc396861f7b5f79ed3a6cc051e155e56e41
Waqar Mohammad <waqar@waqar.co>
2018-12-19 13:40:06 +0000

    Solved sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (typeof num1 == 'string' || typeof num2 == 'string') {
+        return 'ERROR';
+    }
 
+    const smallest = Math.min(num1, num2);
+    const largest = Math.max(num1, num2);
+
+    var number = (largest - smallest) + 1;
+    // to work out sum of numbers between two ints formula == n * (max + min) / 2, where n is the length of the series.
+    number = number * (largest + smallest) / 2;
+
+    if (number < 0 || !Number.isInteger(number)){
+        return 'ERROR';
+    } else {
+        return number;
+    }
 }
 
 module.exports = sumAll
