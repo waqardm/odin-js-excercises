f79a1f6c4171a1bd3abd05a0558488d4c381c9ea
Waqar Mohammad <waqar@waqar.co>
2018-12-19 14:50:03 +0000

    Completed tempConversion - all js TDD done this round

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  // (32°F − 32) × 5/9 = 0°C
+  var convertedTemp = (far - 32) * 5 / 9;
+  return Math.round(convertedTemp * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  // 0°C × 9/5) + 32 = 32°F
+  var convertedTemp = cel * (9/5) + 32;
+  return Math.round(convertedTemp * 10) / 10;
 }
 
 module.exports = {
